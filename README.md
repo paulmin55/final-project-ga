1d670585ec29f53d4476b7cacdcaf8b737ccb0a8
paulmin55 <pmin79@gmail.com>
2020-03-18 22:38:34 -0400

    some refactoring to render graphs

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Queries [SpaceX API](https://api.spacexdata.com/v3/launches) for historical flig
 ## Getting Started
 ### Requirements
 * python3
+* docker (optional)
 
 ### Running the application
 ```
@@ -14,7 +15,7 @@ $ pip install -r requirements.txt
 $ python -m flask run
 ```
 
-### Running on docker
+### Building and running docker image
 ```
 $ docker build . -t <repo>/<image_name>:<tag>
 $ docker run --rm -dp 5000:5000 <repo>/<image_name>:<tag>
@@ -24,6 +25,7 @@ $ docker run --rm -dp 5000:5000 <repo>/<image_name>:<tag>
 * index: [localhost:5000](http://localhost:5000)
 * search by `flight number`: [localhost:5000/flight](http://localhost:5000/flight)
 * search by `mission name`: [localhost:5000/mission](http://localhost:5000/mission)
+* view `statistics`: [localhost:5000/statistics](http://localhost:5000/statistics)
 
 ### Notes
 * App will use an example SpaceX API JSON dump file [spacex_launch_data.json](spacex_launch_data.json) in case there's no network connectivity.
--- app.py
+++ app.py
@@ -5,9 +5,14 @@ from pandas import pandas, concat, json_normalize
 
 import io
 import base64
-import matplotlib
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.rcParams['font.size'] = 10
+plt.gcf().subplots_adjust(left=0.20)
 enable_csrf = False
 http_methods = ('GET', 'POST')
 
@@ -21,10 +26,12 @@ def query():
     if request.method == 'POST':
         if form.query_type.data == default_choice:
             redirect_page = 'flight'
-        elif form.query_type.data =='statistics':
+        elif form.query_type.data == 'mission_name':
+            redirect_page = 'mission'
+        elif form.query_type.data == 'statistics':
             redirect_page = 'statistics'
         else:
-            redirect_page = 'mission'
+            redirect_page = 'query'
         return redirect(url_for(redirect_page))
     return render_template(html_file, form=form)
 
@@ -74,23 +81,36 @@ def mission():
                                 request_method=request.method)
     return render_template(html_file, form=form)
 
+
 @app.route('/statistics', methods=http_methods)
 def statistics():
-    """ show statistics """
+    """ Route for launch statistics """
     html_file = 'statistics.html'
+    flights_by_rocket_stats = flights_by_rocket()
+    return render_template(html_file, flights_by_rocket_stats=flights_by_rocket_stats)
+
+
+def flights_by_rocket():
+    """ return flights by rocket graph """
     launch_data = Launch().get_data()
-    rocket_info =  launch_data[['rocket']].to_dict(orient='index')
+    rocket_info = launch_data[['rocket']].to_dict(orient='index')
     rocket_types = []
+
     for _, value in rocket_info.items():
         rocket_types.append(value['rocket']['rocket_name'])
-    rocket_type_series = pandas.Series(rocket_types).value_counts().plot(kind='bar').get_figure()
-    buffer_obj = io.BytesIO()
-    rocket_type_series.savefig(buffer_obj, format='png')
-    buffer_obj.seek(0)
-    buffer = b''.join(buffer_obj)
-    buffer_encoded = base64.b64encode(buffer)
-    figure = buffer_encoded.decode('utf-8')
-    return render_template(html_file, rocket_types = figure)
+
+    rocket_type_series = pandas.Series(rocket_types)
+    rocket_type_counts = rocket_type_series.value_counts()
+    rocket_type_count_plot = rocket_type_counts.plot(kind='barh').get_figure()
+    rocket_count_list = list(rocket_type_counts)
+    flights_by_rocket_stats = configure_graph(
+                                    series_plot=rocket_type_count_plot,
+                                    title='Number of Flights by Rocket Type',
+                                    x_label='Flights',
+                                    y_label='Type',
+                                    count_values=rocket_count_list)
+    return flights_by_rocket_stats
+
 
 def process_dataframe(result_df):
     """ Logic to create combine dataframe based on
@@ -111,4 +131,21 @@ def process_dataframe_links(result_df):
     """ Extracts links """
     links = json_normalize(result_df.iloc[0]['links'])
     return links
-    
+
+
+def configure_graph(series_plot, title, x_label, y_label, count_values):
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.title(title)
+
+    rocket_count_list = list(count_values)
+    for rocket_type, rocket_type_count in enumerate(rocket_count_list):
+        plt.text(rocket_type_count, rocket_type, str(rocket_type_count))
+
+    buffer_obj = io.BytesIO()
+    series_plot.savefig(buffer_obj, format='png')
+    buffer_obj.seek(0)
+    buffer = b''.join(buffer_obj)
+    buffer_encoded = base64.b64encode(buffer)
+    buffer_decoded = buffer_encoded.decode('utf-8')
+    return buffer_decoded
